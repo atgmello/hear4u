3a9820d087dbcc26841ca25ef65949723a1eb056
Student <student@student2.local>
2019-09-13 13:28:50 -0300

    feat: first commit

---
+++ Hear4uApp/Model/Audio.swift
@@ -0,0 +1,24 @@
+//
+//  Audio.swift
+//  Hear4uApp
+//
+//  Created by Student on 13/09/19.
+//  Copyright © 2019 USP. All rights reserved.
+//
+
+import Foundation
+class Audio {
+    
+    let nome:String
+    let arquivo_de_audio:String
+    let alerta:String
+    
+    
+    init(nome:String, arquivo_de_audio:String, alerta:String) {
+        self.nome = nome
+        self.arquivo_de_audio = arquivo_de_audio
+        self.alerta = alerta
+    }
+    
+    
+}
---
+++ Hear4uApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hear4uApp
+//
+//  Created by Student on 12/09/19.
+//  Copyright © 2019 USP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
